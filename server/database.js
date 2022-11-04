34ca2d339c5400d6179ce28bf592ad6c67fe0d4b
Matt Stoney <mstoney@csumb.edu>
2022-11-04 15:17:20 -0700

    Add models for DB tables, some CRUD functions

--- server/database.js
+++ server/database.js
@@ -1,8 +1,11 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
-const sequelize = new Sequelize('opencommune', 'root', '<<ENTER_YOUR_MYSQL_PASSWORD_HERE>>', {
+const sequelize = new Sequelize('opencommune', 'root', 'Phrygian145', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+      timestamps: false
+    }
   });
 
 // This function will print out if you are connecting to your local DB successfully
@@ -25,7 +28,7 @@ User.init({
   userid: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV1,
-    primaryKey: false
+    primaryKey: true
   },
   username: {
     type: DataTypes.STRING,
@@ -47,27 +50,179 @@ User.init({
     type: DataTypes.STRING,
     allowNull: false
   }
-
-
 }, {
   sequelize,
   modelName: 'User',
   tableName: 'users'
 });
 
+class ListedItem extends Model {}
+
+ListedItem.init({
+  itemid: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+
+  },
+  itemname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  information: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  userlisted: {
+    type: DataTypes.UUID,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'ListedItem',
+  tableName: 'listeditems'
+})
+
+class Network extends Model {};
+
+Network.init({
+  networkid: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  networkname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  networkadmin: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV1
+  },
+}, {
+  sequelize,
+  modelName: 'Network',
+  tableName: 'networks'
+})
+
+class PurchaseHistory extends Model {}
+
+PurchaseHistory.init({
+  purchaseid: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  datesold: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  pricesold: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  userlisted: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  purchaser: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  itemid: {
+    type: DataTypes.INTEGER
+  }
+
+}, {
+  sequelize,
+  modelName: 'PurchaseHistory',
+  tableName: 'purchasehistory'
+})
 
 // This is just a simple query to get all users
-async function getUsers() {
+async function getItems() {
   try {
 
-    const users = await User.findAll({
-      attributes: ['userid', 'username', 'firstname', 'lastname', 'email']
+    const items = await ListedItem.findAll({
+      attributes: ['itemid', 'itemname']
     });
-    console.log(users.every(user => user instanceof User));
-    console.log("All users: ", JSON.stringify(users, null, 2));
+    console.log(items.every(item => item instanceof ListedItem));
+    console.log("All items: ", JSON.stringify(items, null, 2));
   } catch(e) {
     console.log(e);
   }
 }
 
-getUsers()
+async function getItem(itemid) {
+  try {
+    const item = await ListedItem.findByPk(5);
+    console.log(item);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+async function createItem(item) {
+  const { itemname, category, information, price, location, image, userlisted } = item;
+  try {
+    await ListedItem.create({
+      itemname: itemname,
+      category: category,
+      information: information,
+      price: price,
+      location: location,
+      image: image,
+      userlisted: userlisted
+    }, {
+      fields: ['itemname', 'category', 'information', 'price', 'location', 'image', 'userlisted']
+    })
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+async function deleteItem(item) {
+  const { itemname, category, information, price, location, image, userlisted } = item;
+
+  ListedItem.destroy({
+    where: {
+      itemname: itemname,
+      category: category,
+      information: information,
+      price: price,
+      location: location,
+      image: image,
+      userlisted: userlisted
+    }
+  });
+}
+
+item = {
+ itemname: "yeezy shoes",
+ category: "clothing",
+ information: "A pair of mint Yeezys",
+ price: 3.50,
+ location: "The Moon",
+ image: "https://images.stockx.com/images/Adidas-Yeezy-Boost-350-V2-Core-Black-Red-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1606319240&q=75",
+ userlisted: '76123458-5c8b-11ed-9b6a-0242ac120002'
+}
+
+deleteItem(item);
+createItem(item);
+getItem(5);
