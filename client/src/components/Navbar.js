90d5d47b5fed144c2a9d43afcc3ef9c484a3cb3b
Matt Stoney <mstoney@csumb.edu>
2022-11-17 22:01:51 -0800

    Add Logo/Banner, Navbar, About-blurb component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function Navbar() {
+  return (
+    <>
+    <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+    </ul>
+    </>
+  )
+}
