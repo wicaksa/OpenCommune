e3b5421345f454bb0a723263b39daf120b118a76
cheukyim <cheukonyim@gmail.com>
2022-11-22 23:12:52 -0800

    login signup & localstorage

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const saved = localStorage.getItem("userid");
+if (!saved){
+  localStorage.setItem("userid", JSON.stringify(null))
+}else{
+  localStorage.setItem("userid", saved)
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
